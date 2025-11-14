13c758f06d91bdefea41b7e9be14b74e717769bb
Gee <alickoamarko@gmail.com>
2025-11-14 19:55:43 +0100

    MVP?

---
+++ GHSServerLauncher.py
@@ -0,0 +1,30 @@
+import miniupnpc
+import os
+
+
+try:
+        upnp = miniupnpc.UPnP()
+        if not upnp.discover():
+                raise RuntimeError("No UPnP devices found")
+        upnp.selectigd()
+        upnp.addportmapping(
+		27015,
+		"UDP",
+		upnp.lanaddr,
+		27015,
+		"Source",
+		""
+	)
+        upnp.addportmapping(
+		27015,
+		"TCP",
+		upnp.lanaddr,
+		27015,
+		"Source",
+		""
+	)
+except Exception as e:
+	print("UPnP ERROR: " + str(e))
+
+
+os.system("start .\\ServerFiles\\srcds.exe -game hidden -tickrate 128")
